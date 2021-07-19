599c1ce4e1ba2f583a0fd70e6025bffb46c42538
SchofieChen <SchofieChen>
2021-07-19 21:28:17 +0800

    first

---
+++ GFC/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SynchronousSocketClient SSClient = new SynchronousSocketClient();
+
+
+            SSClient.StartClient();
+            Console.Read();
+
+        }
+
+
+    }
+    public class SynchronousSocketClient
+    {
+        public byte[] addByteToArray(byte[] bArray, byte newByte)
+        {
+            byte[] newArray = new byte[bArray.Length + 1];
+            bArray.CopyTo(newArray, 1);
+            newArray[0] = newByte;
+            return newArray;
+        }
+
+        public void StartClient()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                IPHostEntry ipHostInfo = Dns.GetHostEntry("127.0.0.1");
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8000);
+
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a Client Message<EOF>");
+
+                    string str = "Get DIAGNOSTIC INFO<EOF>";
+                    byte[] array = Encoding.ASCII.GetBytes(str);
+
+                    byte[] newarray = addByteToArray(array, 0x00);
+                    byte[] newarray2 = addByteToArray(newarray, 0x13);
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(newarray2);
+
+                    // Receive the response from the remote device.  
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.  
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
